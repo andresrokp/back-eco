f37c5a28cdc4e5f10f5298fcd623898d0849055c
andresropk <afrokap@gmail.com>
2023-05-30 18:42:53 -0500

    feat: serving a html file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 
-const readRegistros = require("./db/registrosGateways");
+// const readRegistros = require("./db/registrosGateways");
 
 
 const app = express();
 app.use(express.json());
+app.use(express.static("./public/index.html"));
 
 
 app.get("/",(rq,rs)=>{
     // rs.send("Hello my people");
-    readRegistros().then((res)=>{rs.send(res);});
+    // readRegistros().then((res)=>{rs.send(res);});
+    rs.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(3000,()=>{
@@ -20,7 +22,7 @@ app.listen(3000,()=>{
 setInterval(() => {
     console.log("Running en esta mondá...");
 }, 2000);
-setInterval(() => {
-    readRegistros().then((res)=>{console.error(res);});
-}, 5000);
+// setInterval(() => {
+//     readRegistros().then((res)=>{console.error(res);});
+// }, 5000);
 
