35052d6fe7a55f7e2d260bad3a3b2dd9da1ca0cb
andresropk <afrokap@gmail.com>
2023-05-30 20:21:16 -0500

    feat: sending public folder and execute JS

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
+"use strict";
+
 require("dotenv").config();
 const express = require("express");
+// const path = require("path");
 
 // const readRegistros = require("./db/registrosGateways");
 
 
 const app = express();
-app.use(express.json());
+// app.use(express.json());
 app.use(express.static("public"));
 
 
-// app.get("/",(rq,rs)=>{
-//     // rs.send("Hello my people");
-//     // readRegistros().then((res)=>{rs.send(res);});
-//     // rs.sendFile(__dirname + "/public/index.html");
-//     // rs.sendFile(__dirname + "/public/script.js");
-//     console.log("Se mandó esa mierda");
-// });
+app.get("/",()=>{
+// readRegistros().then((res)=>{rs.send(res);});
+// rs.sendFile(__dirname + "/public/index.html");
+});
 
 app.listen(3000,()=>{
     console.log("está escuchando esta cagá");
