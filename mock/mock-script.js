273fec235ff4bbfb7b319c86563e530bb9433553
andresrokp <afrokap@gmail.com>
2023-12-22 22:20:06 -0500

    feat: automatic array traversing direction swap

--- mock/mock-script.js
+++ mock/mock-script.js
@@ -1,15 +1,28 @@
 const dataArray = require("./data.json");
 
 let index = 0;
-
+let isRising = false;
 
 function getElement() {
+    
+    console.log('index out',index);
     const value = dataArray[index];
-    index++;
+    // if reaches endpoints, invert iteration course
+    if ( (index == 0) || (index == dataArray.length - 1) ) isRising = !isRising;
+    // depending on direction, add or less
+    isRising ? index++ : index--;
+    
     return value
 }
 
-const element = getElement();
-console.log(element);
 
 module.exports = getElement;
+
+// -------  QUICK TEST
+
+(async ()=>{
+    for (let i of dataArray.concat(dataArray)) {
+        const element = getElement();
+        console.log(element.index);
+    }
+})()
