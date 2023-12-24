3d4fd6e1eb1ef1c6fe4e59654a21baba7f1dac7b
andresrokp <afrokap@gmail.com>
2023-12-24 16:51:02 -0500

    feat: serving indexed object on endpoint :)

--- index-eco.js
+++ index-eco.js
@@ -5,6 +5,11 @@ const express = require("express");
 
 const {readRegistros,readJoinCruzado,ultimaVistaRec} = require("./db/registrosGateways");
 
+const {getElementSpecific,getElementAutomatic,getCsvData} = require("./mock/mock-script");
+{
+
+};
+
 
 const app = express();
 // app.use(express.json());
@@ -36,6 +41,15 @@ app.get("/joins/ultima-vista",(rq,rs)=>{
             // console.table(res);
         });
     console.log("sending :: ultima vista de cada tag...");
+});
+
+app.get("/mock-api/casabe/:index", async (req,res)=>{
+    const index = parseInt(req.params.index);
+    getCsvData()
+    .then((array)=>{
+        res.json(array[index]);
+        console.log("sending :: ", array[index]);
+    })
 });
 
 app.listen(process.env.PORT || 3000,()=>{
