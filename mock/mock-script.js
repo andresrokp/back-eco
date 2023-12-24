e54e72999cb561e2bdee0cd6877d24c233802d4b
andresrokp <afrokap@gmail.com>
2023-12-24 16:00:36 -0500

    feat: reading csv file

--- mock/mock-script.js
+++ mock/mock-script.js
@@ -1,13 +1,42 @@
-const dataArray = require("./data.json");
+const fs = require("fs");
+const csv = require("csv-parser");
+
+// FILE READING -----
+
+// json
+const dataArrayJson = require("./data.json");
+// csv
+const dataArrayCsv = [];
+async function loadCsvData(){
+    await new Promise((resolve, reject) => {
+        fs.createReadStream("./mock/data.csv")
+        .pipe(csv({ headers: true })) // Consider headers row
+        .on("data", (row) => {
+            dataArrayCsv.push(row)
+        })
+        .on("end", () => {
+            resolve()
+        })
+        .on("error", (error) => {
+            reject(error);
+        });
+    })
+};
+
+async function getCsvData() {
+    if (dataArrayCsv.length == 0) await loadCsvData();
+    return dataArrayCsv;
+}
+
+
 
 let index = 0;
 let isRising = false;
 
-
 function getElementSpecific(index) {
     
     try {
-        return value = dataArray[index];
+        return dataArrayJson[index];
     } catch (error) {
         return 400;
     }
@@ -16,10 +45,10 @@ function getElementSpecific(index) {
 
 function getElementAutomatic() {
     
-    console.log('index out',index);
-    const value = dataArray[index];
+    // console.log("index out",index);
+    const value = dataArrayJson[index];
     // if reaches endpoints, invert iteration course
-    if ( (index == 0) || (index == dataArray.length - 1) ) isRising = !isRising;
+    if ( (index == 0) || (index == dataArrayJson.length - 1) ) isRising = !isRising;
     // depending on direction, add or less
     isRising ? index++ : index--;
     
@@ -27,16 +56,21 @@ function getElementAutomatic() {
 }
 
 
+
+
 module.exports = {
     getElementSpecific,
-    getElementAutomatic
-}
+    getElementAutomatic,
+    getCsvData
+};
 
 // -------  QUICK TEST
 
+// for (let i of dataArrayJson.concat(dataArrayJson)) {
+//     getElementAutomatic();
+//     i;
+// }
+
 (async ()=>{
-    for (let i of dataArray.concat(dataArray)) {
-        const element = getElementAutomatic();
-        console.log(element.index);
-    }
+    console.log(await getCsvData());
 })()
